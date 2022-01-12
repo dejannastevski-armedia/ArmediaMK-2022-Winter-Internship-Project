248aba8a7c1c932844bcb4c233f6155b45635b49
ARMEDIA\hristijan.slavkoski <kiko_slavkoski@yahoo.com>
2022-01-12 08:03:26 -0800

    Requested changes done

--- project/First-Project/src/main/java/first/project/controller/HelloController.java
+++ project/First-Project/src/main/java/first/project/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController
 {
 
-    @RequestMapping(value = "/HelloWorld", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
     @ResponseBody
     String index()
     {
