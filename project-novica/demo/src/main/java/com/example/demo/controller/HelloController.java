ce74130fd697379b58b405726e4ab54aa9982961
ARMEDIA\novica.veljanoski <novica.veljanoski@students.finki.ukim.mk>
2022-01-12 02:50:55 -0800

    Hello World controller added

---
+++ project-novica/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/helloworld")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
