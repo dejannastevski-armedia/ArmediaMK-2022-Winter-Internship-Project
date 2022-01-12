5b4bb2fc47d6a24836be1d725cfc9a996cc9bb08
ARMEDIA\david.anastasov <anastasovdavid@gmail.com>
2022-01-12 11:49:49 -0800

    helloworld controller

--- project/firstproject/src/main/java/com/example/firstproject/FirstprojectApplication.java
+++ project/firstproject/src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstprojectApplication {
-
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		SpringApplication.run(FirstprojectApplication.class, args);
 	}
-
 }
