84cd1ddaa0f3429317121ffc3e90bc755b53d9b2
Marija Mitreska <marija.mitreska@armedia.com>
2022-01-13 17:32:40 +0100

    Formatted cod new

--- Project/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Project/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,21 @@
-    package com.example.demo;
+package com.example.demo;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.boot.CommandLineRunner;
-    import org.springframework.boot.SpringApplication;
-    import org.springframework.boot.autoconfigure.SpringBootApplication;
-    import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
+public class DemoApplication
+{
 
+    public static void main(String[] args)
+    {
 
-    @SpringBootApplication
-    public class DemoApplication {
-
-        public static void main(String[] args) {
-
-            SpringApplication.run(DemoApplication.class, args);
-        }
-
-
+        SpringApplication.run(DemoApplication.class, args);
     }
+
+
+}
