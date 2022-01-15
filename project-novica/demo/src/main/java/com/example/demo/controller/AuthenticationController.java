7f7dcf42c9e1dbca11e546931e39555f9272578a
ARMEDIA\novica.veljanoski <novica.veljanoski@students.finki.ukim.mk>
2022-01-15 12:33:08 -0800

    delete user option added

--- project-novica/demo/src/main/java/com/example/demo/controller/AuthenticationController.java
+++ project-novica/demo/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -1,16 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
 import com.example.demo.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,7 +23,7 @@ public class AuthenticationController
     public String viewHomePage(Model model)
     {
         model.addAttribute("user",new  User());
-        return "index";
+        return "login";
     }
 
     @GetMapping("/register")
@@ -38,7 +37,7 @@ public class AuthenticationController
     @GetMapping("/login")
     public String getLoginPage(Model model){
         model.addAttribute("user",new  User());
-        return "index";
+        return "login";
     }
 
     @PostMapping("/login")
@@ -47,15 +46,21 @@ public class AuthenticationController
         model.addAttribute("user", new User());
         authenticationService.save(user);
 
-        return "index";
+        return "login";
     }
 
-//    @GetMapping("/index")
-//    public String getIndexPage(Model model)
-//    {
-//        model.addAttribute("user", new User());
-//        return "index";
-//    }
+////    @GetMapping("/index")
+////    public String getIndexPage(Model model)
+////    {
+////        model.addAttribute("user", new User());
+////        return "index";
+////    }
+
+    @PostMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable Long id){
+        this.authenticationService.deleteUserById(id);
+        return "redirect:/users";
+    }
 
     @GetMapping("/users")
     public String listUsers(Model model)
@@ -65,4 +70,5 @@ public class AuthenticationController
 
         return "users";
     }
+
 }
