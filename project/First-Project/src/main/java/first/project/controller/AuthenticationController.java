1443a25d89951cb43c72c070c5b515ac72c0eb7c
ARMEDIA\hristijan.slavkoski <kiko_slavkoski@yahoo.com>
2022-01-17 06:47:08 -0800

    User Register - Home Controller Default Route Move To HelloController

--- project/First-Project/src/main/java/first/project/controller/AuthenticationController.java
+++ project/First-Project/src/main/java/first/project/controller/AuthenticationController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 
 @Controller
+@RequestMapping(value = "/auth")
 public class AuthenticationController
 {
     @Autowired
