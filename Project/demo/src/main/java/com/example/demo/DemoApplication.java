664af77f9f583e29221390d2ddc0a59b1c253e22
Marija Mitreska <marija.mitreska@armedia.com>
2022-01-18 11:16:33 +0100

    Resolving conflicts

--- Project/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Project/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,5 @@
 package com.example.demo;
-<<<<<<< HEAD
-=======
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
->>>>>>> dca96b3aced208d8c12d75805234b2ebe0e415ad
-
-@SpringBootApplication
-public class DemoApplication
-{
-
-<<<<<<< HEAD
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -23,15 +8,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class DemoApplication {
 
-    public static void main(String[] args) {
-
-=======
     public static void main(String[] args)
     {
->>>>>>> dca96b3aced208d8c12d75805234b2ebe0e415ad
-
         SpringApplication.run(DemoApplication.class, args);
     }
-
-
 }
--- Project/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ Project/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //        package com.example.demo.controller;
 //
 //        import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +12,3 @@
 //                return "Hello World";
 //            }
 //        }
-=======
-package com.example.demo.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-    @RequestMapping("/hello")
-    public String helloWorld()
-    {
-        return "Hello World";
-    }
-}
->>>>>>> dca96b3aced208d8c12d75805234b2ebe0e415ad
