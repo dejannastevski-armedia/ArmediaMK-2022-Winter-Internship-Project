14c886b12748a660f877e7741c2678787e4c832a
ARMEDIA\novica.veljanoski <novica.veljanoski@students.finki.ukim.mk>
2022-01-20 05:43:47 -0800

    Ajax solved

--- project-novica/demo/src/main/java/com/example/demo/controller/AuthenticationController.java
+++ project-novica/demo/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -1,15 +1,18 @@
 package com.example.demo.controller;
 
+import antlr.StringUtils;
 import com.example.demo.model.User;
 import com.example.demo.service.AuthenticationService;
 import com.example.demo.util.PasswordHashing;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller()
 @RequestMapping("/auth")
@@ -49,22 +52,33 @@ public class AuthenticationController
         model.addAttribute("user",new  User());
         return "login";
     }
-
-    @PostMapping("/login")
-    public String loginUser(@RequestParam String email,
-                            @RequestParam String password,
-                            Model model)
-    {
-        ArrayList<String>list= (ArrayList<String>) authenticationService.login(email,password);
-        if(list.isEmpty()){
-            return "redirect:/home";
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> logIn (String email, String password){
+        List<String> res=authenticationService.login(email,password);
+        if(res.size()==0){
+            return ResponseEntity.ok("success");
         }
-        else {
-            model.addAttribute("error", list);
-            model.addAttribute("user", new User());
-            return "login";
+        else{
+            return ResponseEntity.badRequest().body(String.join(", ",res));
         }
     }
+
+//    @PostMapping("/login")
+//    public String loginUser(@RequestParam String email,
+//                            @RequestParam String password,
+//                            Model model)
+//    {
+//        ArrayList<String>list= (ArrayList<String>) authenticationService.login(email,password);
+//        if(list.isEmpty()){
+//            return "redirect:/home";
+//        }
+//        else {
+//            model.addAttribute("error", list);
+//            model.addAttribute("user", new User());
+//            return "login";
+//        }
+//    }
 
     @PostMapping("/register")
     public String registerUser(@RequestParam String email ,
--- project-novica/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ project-novica/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.User;
+import com.example.demo.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController
 {
+    @Autowired
+    AuthenticationService authenticationService;
 
     @RequestMapping("/home")
     public String helloWorld(Model model)
@@ -16,4 +23,15 @@ public class HelloController
         return "home";
     }
 
+//    @RequestMapping(path = "/login", method = RequestMethod.POST)
+//    @ResponseBody
+//    public ResponseEntity<String> add (User user){
+//        String res= String.valueOf(authenticationService.login(user.getEmail(), user.getPassword()));
+//        if(/*authenticationService.login(user.getEmail(), user.getPassword()).isEmpty()/*/res.length()==0){
+//            return ResponseEntity.ok("success");
+//        }
+//        else{
+//           return ResponseEntity.badRequest().body("Please enter valid credentials");
+//        }
+//    }
 }
