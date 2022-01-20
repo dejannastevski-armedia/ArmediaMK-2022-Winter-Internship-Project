b77744871e97da87bcabeea3db64d2b5ab981e40
Marija Mitreska <marija.mitreska@armedia.com>
2022-01-20 16:34:12 +0100

    Login User

---
+++ Project/demo/src/main/resources/static/postRequest.js
@@ -0,0 +1,18 @@
+
+    function login(){
+    var userdto = {};
+    userdto.email = $("#email").val();
+    userdto.password = $("#password").val();
+    $.ajax({
+    type:"POST",
+    contentType:"application/json",
+    url:"/login",
+    data:JSON.stringify(userdto),
+    success: function(data){
+    window.location.replace("http://localhost:8080/home");
+},
+    error: function (e){
+    document.getElementById("errorMessage").innerText=e.responseText;
+}
+})
+}
