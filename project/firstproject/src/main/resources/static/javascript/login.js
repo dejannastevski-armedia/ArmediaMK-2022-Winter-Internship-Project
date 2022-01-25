bedf0eb8b65b77d12b5cc09d1e5b3e0470211904
ARMEDIA\david.anastasov <anastasovdavid@gmail.com>
2022-01-25 13:23:33 -0800

    seperated login & questions javascript file

--- project/firstproject/src/main/resources/static/javascript/login.js
+++ project/firstproject/src/main/resources/static/javascript/login.js
@@ -17,21 +17,3 @@ function loginUser()
         }
     });
 }
-
-function saveQuestion() {
-    var questionDTO = {};
-    questionDTO.title = $("#title").val();
-    questionDTO.question = $("#questionform").val();
-    $.ajax({
-        type: "POST",
-        contentType: "application/json",
-        url: "/question/save",
-        data: JSON.stringify(questionDTO),
-        success: function () {
-            window.location.replace("http://localhost:8080/auth/logged-in");
-        },
-        error: function (e) {
-            document.getElementById("questionsErrorMessage").innerHTML = e.responseText;
-        }
-    });
-}
