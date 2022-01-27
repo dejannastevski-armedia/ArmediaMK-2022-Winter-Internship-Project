4e7f087c4781b4fa02c0afeb9f0d097df0652367
Marija Mitreska <marija.mitreska@armedia.com>
2022-01-27 18:45:24 +0100

    Changed Answer Controller

--- Project/demo/src/main/java/com/example/demo/controller/AnswerController.java
+++ Project/demo/src/main/java/com/example/demo/controller/AnswerController.java
@@ -1,16 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AnswerController
 {
-
     @GetMapping("/view-answer/{id}")
-    public String viewAnswer(@PathVariable("id") Long id)
+    public String viewAnswer(@PathVariable("id") Long id, Model model)
     {
+        model.addAttribute("questionId", id);
         return "view_answer";
     }
 }
