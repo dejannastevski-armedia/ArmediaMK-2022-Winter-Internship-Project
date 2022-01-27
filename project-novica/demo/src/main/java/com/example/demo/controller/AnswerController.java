2c7f5f37dd59db2a1e7a60ba05649300ca55c785
ARMEDIA\novica.veljanoski <novica.veljanoski@students.finki.ukim.mk>
2022-01-27 12:16:04 -0800

    Path variale id solved

--- project-novica/demo/src/main/java/com/example/demo/controller/AnswerController.java
+++ project-novica/demo/src/main/java/com/example/demo/controller/AnswerController.java
@@ -2,12 +2,15 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/answer")
 public class AnswerController
 {
-    @GetMapping("/answer")
-    public String AnswerPage()
+    @GetMapping("/answer/{id}")
+    public String AnswerPage(@PathVariable long id)
     {
         return "answer";
     }
