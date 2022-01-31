42234145290519f05d59bba8eea4bb2464672f4c
ARMEDIA\david.anastasov <anastasovdavid@gmail.com>
2022-01-31 12:22:39 -0800

    list answers table added

--- project/firstproject/src/main/java/com/example/firstproject/services/AnswerService.java
+++ project/firstproject/src/main/java/com/example/firstproject/services/AnswerService.java
@@ -2,9 +2,13 @@ package com.example.firstproject.services;
 
 import com.example.firstproject.model.Answer;
 
+import java.util.List;
+
 public interface AnswerService
 {
     String validateAnswer(String answer);
     void saveAnswer(Answer answer);
     Answer createAnswer(String answer, String email, Long questionId);
+
+    List<Answer> listAllAnswers(Long questionId);
 }
