0b1c8d906681ea0b8d1d334bfd50793acad3a003
ARMEDIA\hristijan.slavkoski <kiko_slavkoski@yahoo.com>
2022-02-02 08:22:16 -0800

    Change of primary key in user_answer_status table, to composite primary key of user_id and answer_id

--- project/First-Project/src/main/java/first/project/model/UserAnswerStatus.java
+++ project/First-Project/src/main/java/first/project/model/UserAnswerStatus.java
@@ -5,9 +5,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -15,32 +14,21 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "userAnswerStatus")
 @EntityListeners(AuditingEntityListener.class)
+@IdClass(UserAnswerIdPrimaryKey.class)
 public class UserAnswerStatus
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
     @ManyToOne
     @JoinColumn(name = "userId", nullable = false)
     private User user;
 
+    @Id
     @ManyToOne
     @JoinColumn(name = "answerId", nullable = false)
     private Answer answer;
 
     @Column(name = "status", nullable = false)
     private boolean status;
-
-    public Integer getId()
-    {
-        return id;
-    }
-
-    public void setId(Integer id)
-    {
-        this.id = id;
-    }
 
     public User getUser()
     {
